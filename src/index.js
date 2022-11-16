e16ddf161ed1fad99e775999257449b2b6655390
Lon Wibberding <wibberding@gmail.com>
2022-11-16 11:33:03 -0800

    Pasrtial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+import cors from "cors";
+import consola from "consola";
+import express from "express";
+import mongoose from "mongoose";
+import { json } from "body-parser";
+
+//Iitialize express application
+const app = express();
+
+// Apply application middlewares
+app.use(cors());
+app.use(json());
+
+// Import application constants.
+import { DB, PORT } from "./constants";
+console.log(DB);
+const main = async () => {
+  try {
+    // Connect with the database.
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    consola.success("Database connected ...")
+    // Start application listening for request from server.
+    app.listen(PORT, () => consola.success(`Server started on port ${PORT}`));
+  } catch (err) {
+    consola.error(`Unable to start the server \n${err.message}`);
+  }
+};
+
---
+++ src/model/User.js
@@ -0,0 +1,68 @@
+import { Schema, model } from "mongoose";
+import {compare, hash} from "bcryptjs";
+import { SECRET } from "../constants";
+import { sign } from "jsonwebtoken";
+import { randomBytes} from "crypto";
+import { pick } from "lodash";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  verificationCode: {
+    type: String,
+    required: false,
+  },
+  resetPasswordToken: {
+    type: String,
+    required: false,
+  },
+  resetPasswordExpiresIn: {
+    type: Date,
+    required: false,
+  }
+
+}, { timestamps: true} );
+
+UserSchema.pre("save", async function (name) {
+  let user = this;
+  if (!user.isModified("password")) return next();
+  user.password = await hash(user.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await compare(password, this.password);
+};
+
+UserSchema.methods.generateJWT = async function () {
+  let payload = {
+    username: this.username,
+    email: this.email,
+    name: this.name,
+    id: this._id,
+  }
+  return await sign(payload, SECRET, { expiresIn: "1 day"});
+}
+
+UserSchema.methods.generatePasswordReset = function () {
+  this.resetPasswordExpiresIn = Date.now() + 36000000;
+  this.resetPasswordToken = randomBytes(20).toString("hex");
+}
