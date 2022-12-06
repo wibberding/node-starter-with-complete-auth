c7df4cad193eedfb84a020293193492fd18df476
Lon Wibberding <wibberding@gmail.com>
2022-12-05 16:38:43 -0800

    Initial auth working

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,5 @@
-import {userAuth} from '../middleware/auth-check';
 
 // Display root page
 exports.index = (req, res, next) => {
-	res.render('account/index', userAuth, {layout: 'main', infoMessage: req.flash('info'), errorMessage: req.flash('error')}); 
+	res.render('account/index', {layout: 'main', infoMessage: req.flash('info'), errorMessage: req.flash('error')}); 
 };
--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,12 +1,22 @@
-import { Schema, model } from "mongoose";
-import {compare, hash} from "bcryptjs";
+// import { Schema, model } from "mongoose";
+// import {compare, hash} from "bcryptjs";
 import { SECRET } from "../constants";
-import { sign } from "jsonwebtoken";
-import { randomBytes} from "crypto";
-import { pick } from "lodash";
+import { verify } from "jsonwebtoken";
+// import { randomBytes} from "crypto";
+// import { pick } from "lodash";
 
-export const userAuth = (req, res, next) => {
+const userAuth = (req, res, next) => {
   console.log('Made it to auth check');
-  console.log(req.cookies.token);
+  let token = req.cookies.token;
+  console.log(token);
+  try {
+    var decoded = verify(token, SECRET);
+    console.log("decoded", decoded);
+  } catch(err) {
+    console.log("error", err);
+  }
   next();
 };
+
+export default userAuth;
+
