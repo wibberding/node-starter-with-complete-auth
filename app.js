13fe657c0bd3b6ac44d634bd4d103524056ce051
Lon Wibberding <wibberding@gmail.com>
2022-12-05 19:38:39 -0800

    Fixes passport error

--- app.js
+++ app.js
@@ -30,10 +30,6 @@ app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
-app.use(passport.initialize());
-// Import Passport middleware
-require("./middleware/passport-middleware");
-
 // Apply application middlewares
 app.use(cors());
 app.use(express.json());
